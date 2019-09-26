a91b555b216db1bee50ef66db18f200ddc2fcfb6
JCC <30018387+jchenche@users.noreply.github.com>
2019-09-25 22:18:16 -0700

    Update longestSubstringWithoutRepeatingChar.py

--- longestSubstringWithoutRepeatingChar.py
+++ longestSubstringWithoutRepeatingChar.py
@@ -3,17 +3,14 @@ class Solution:
         hashmap = {}
         longest = 0
         left = 0
-        right = 0
         for i in range(len(s)):
-            if right - left > longest:
-                longest = right - left
+            right = i
             x = s[i]
             # 1st, makes sure it's seen before
             # 2nd, makes sure it's within the window defined by left and right
             if x in hashmap and hashmap[x] >= left:
                 left = hashmap[x] + 1
-            hashmap[x] = i
-            right += 1
-        if right - left > longest:
-            longest = right - left
+            hashmap[x] = right
+            if right - left  + 1 > longest:
+                longest = right - left + 1
         return longest
